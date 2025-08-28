3688e6de22be43766b97de16289841e78abafebf
Ajith P <163892044+ajithhh000@users.noreply.github.com>
2025-08-28 22:05:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/portfolio-website/' : '/',
+  base: mode === 'production' ? '/portfolio/' : '/',
   server: {
     host: "::",
     port: 8080,
